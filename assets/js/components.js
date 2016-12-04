759319aa3169112b113cf8d2ee5bea1dce65d3c7
Long77 <longjoshua77@gmail.com>
2016-12-04 14:54:27 -0600

    redux, lost and found css

--- assets/js/components.js
+++ assets/js/components.js
@@ -15,9 +15,9 @@ export const Nav = ({includeLogin}) =>
         <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
+            <span className="icon-bar">Home</span>
+            <span className="icon-bar">About</span>
+            <span className="icon-bar">Register</span>
             </button>
             <a className="navbar-brand" href="#">Stably.us</a>
         </div>
@@ -29,13 +29,12 @@ export const Nav = ({includeLogin}) =>
 
 export const Jumbotron = () => 
     <div className="jumbotron">
-        <div className="container">
-            <hr/>
+        <div className="container">        
             <h1>Welcome to Stably!</h1>
             <p>The app that keeps you on track.</p>
             <p>Our focus is YOUR focus. This program keeps track of how much time you've spent working, and motivates you with milestones and rewards.
             If you work in tech or use your computer for business, it will manage your billable hours, breaks, and much more. Create your profile below!</p>
-            <p><a className="btn btn-primary btn-lg" href="#" role="button">Get Started! &raquo;</a></p>
+            <p><a className="btn btn-primary btn-lg" href="#/register" role="button">LogIn or Register Here! &raquo;</a></p>
         </div>
     </div>
 
@@ -47,7 +46,7 @@ export const HomeContents = () =>
                 <p>Stably has the potential to enhance your awareness, dexterity, and productivity while using the computer.
                 Perfect for typists, programmers, data analysts, number crunchers, teachers, bloggers - you name it!</p>
                 <p><b> Wherever you go, whatever you do with your machine, Stably can go the distance with you.</b></p>
-                <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                <p><a className="btn btn-default" href="#" role="button">Find Out How &raquo;</a></p>
             </div>
             
             <div className="col-md-4">
@@ -57,14 +56,14 @@ export const HomeContents = () =>
                 1. The Standard timer counts your minutes as long as you want it to, and suggests breaks as you go.
                 <br/>
                 2. For short bursts or time-sensitive duties, we've also included a built-in Pomodoro Timer (Read the benefits of the "Tomato" technique here) allowing you to dictate your workflow.
-                <br/>
-                With every completed session, Stably learns how and when you work best. The app will suggest a schedule for you to be the most productive based on your habits!</p>
+                <br/></p>
+                <p><b>With every completed session, Stably learns how and when you work best. The app will suggest a schedule for you to be the most productive based on your habits!</b></p>
                 <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
             </div>
 
             <div className="col-md-4">
-                <h2></h2>
-                <p></p>
+                <h2>Conquer your Wanderlust</h2>
+                <p>Coming Soon: Stably will have the ability to suggest events and locales near you. We are working on bringing fresh and new activities which connect you with the local culture. Stay tuned!</p>
                 <p><a className="btn btn-default" href="#" role="button">View details &raquo;</a></p>
             </div>
         </div>
