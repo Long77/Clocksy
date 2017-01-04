47f8b592273ce5465badb32de04bf7124ea7e636
Long77 <longjoshua77@gmail.com>
2017-01-04 14:09:54 -0600

    test new file saved

--- assets/js/components.js
+++ assets/js/components.js
@@ -28,7 +28,6 @@ export const Nav = ({includeLogin}) =>
             </div>
         </div>
     </nav>
-    // renamed the file. test test
 
 export const Jumbotron = () => 
     <div className="jumbotron">
